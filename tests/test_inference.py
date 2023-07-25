1d2a93467b7e2d292b44d762d006bcfb8ac3715e
hastur66 <chamathkaubandara@gmail.com>
2023-07-26 01:05:54 +0530

    disable inference unit test

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,20 +1,31 @@
-from micronus.transformer_model import TransformerModel
-from micronus.inference import Infer
+# from micronus.transformer_model import TransformerModel
+# from micronus.inference import Infer
 
-inferencing_model = TransformerModel()
+# inferencing_model = TransformerModel()
 
-# Sentence to translate
-sentence = ['im thirsty']
+# # Sentence to translate
+# # sentence = ['im thirsty']
 
-# Load the trained model's weights at the specified epoch
-inferencing_model.load_weights('weights/wghts16.ckpt')
+# # # Load the trained model's weights at the specified epoch
+# # inferencing_model.load_weights('weights/wghts16.ckpt')
  
 
-class TestInference():
+# class TestInfer():
+#     # def test_init(self):
+#     #     # Create a new instance of the 'Infer' class
+#     #     translator = Infer(inferencing_model)
+#     #     assert isinstance(translator, Infer)
 
-    def test_call(self):
-        # Create a new instance of the 'Infer' class
-        translator = Infer(inferencing_model)
+    
+#     # def test_call(self):
+#     #     # Create a new instance of the 'Infer' class
+#     #     translator = Infer(inferencing_model)
         
-        # Translate the input sentence
-        print(translator(sentence))
+#     #     # Translate the input sentence
+#     #     print(translator(sentence))
+
+#     def test_load_tokenizer(self):
+#         # Create a new instance of the 'Infer' class
+#         translator = Infer(inferencing_model)
+#         translator.load_tokenizer()
+#         assert translator.tokenizer is not None
