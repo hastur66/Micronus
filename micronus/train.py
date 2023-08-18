79323998598b82695b25dd22f8d61ee6f164a2fe
hastur66 <chamathkaubandara@gmail.com>
2023-08-18 21:03:27 +0530

    add argparse to train module

--- micronus/train.py
+++ micronus/train.py
@@ -8,25 +8,61 @@ from micronus.transformer_model import TransformerModel
 from micronus.data_pipeline import PrepareDataset
 from time import time
 from pickle import dump
+import argparse
  
- 
+parser = argparse.ArgumentParser(description="micronus training parameters", 
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--h", type=int, default=8, help="Number of self-attention heads")
+parser.add_argument("--d_k", type=int, default=64, help="Dimensionality of the linearly projected queries and keys")
+parser.add_argument("--d_v", type=int, default=64, help="Dimensionality of the linearly projected values")
+parser.add_argument("--d_model", type=int, default=512, help="Dimensionality of model layers' outputs")
+parser.add_argument("--d_ff", type=int, default=2048, help="Dimensionality of the inner fully connected layer")
+parser.add_argument("--n", type=int, default=6, help="Number of layers in the encoder stack")
+
+parser.add_argument("--epochs", type=int, default=20, help="Number of epochs to train for")
+parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
+parser.add_argument("--beta_1", type=float, default=0.9, help="Adam optimizer beta 1")
+parser.add_argument("--beta_2", type=float, default=0.98, help="Adam optimizer beta 2")
+parser.add_argument("--epsilon", type=float, default=1e-9, help="Adam optimizer epsilon")
+parser.add_argument("--dropout_rate", type=float, default=0.1, help="Dropout rate")
+
+parser.add_argument("--dataset", type=str, default="english-german.pkl", help="Name of the dataset file")
+
+args = vars(parser.parse_args())
 # Define the model parameters
-h = 8  # Number of self-attention heads
-d_k = 64  # Dimensionality of the linearly projected queries and keys
-d_v = 64  # Dimensionality of the linearly projected values
-d_model = 512  # Dimensionality of model layers' outputs
-d_ff = 2048  # Dimensionality of the inner fully connected layer
-n = 6  # Number of layers in the encoder stack
- 
+# h = 8  # Number of self-attention heads
+# d_k = 64  # Dimensionality of the linearly projected queries and keys
+# d_v = 64  # Dimensionality of the linearly projected values
+# d_model = 512  # Dimensionality of model layers' outputs
+# d_ff = 2048  # Dimensionality of the inner fully connected layer
+# n = 6  # Number of layers in the encoder stack
+
+# epochs = 20
+# batch_size = 64
+# beta_1 = 0.9
+# beta_2 = 0.98
+# epsilon = 1e-9
+# dropout_rate = 0.1
+
+
+h = args["h"]
+d_k = args["d_k"]
+d_v = args["d_v"]
+d_model = args["d_model"]
+d_ff = args["d_ff"]
+n =  args["n"]
+
 # Define the training parameters
-epochs = 20
-batch_size = 64
-beta_1 = 0.9
-beta_2 = 0.98
-epsilon = 1e-9
-dropout_rate = 0.1
+epochs = args["epochs"]
+batch_size = args["batch_size"]
+beta_1 = args["beta_1"]
+beta_2 = args["beta_2"]
+epsilon = args["epsilon"]
+dropout_rate = args["dropout_rate"]
  
-DATASET='english-german.pkl'
+# DATASET='english-german.pkl'
+DATASET = args["dataset"]
  
 # Implementing a learning rate scheduler
 class LRScheduler(LearningRateSchedule):
@@ -226,66 +262,83 @@ def train_step(encoder_input, decoder_input, decoder_output):
     train_loss(loss)
     train_accuracy(accuracy)
  
- 
-for epoch in range(epochs):
- 
-    train_loss.reset_states()
-    train_accuracy.reset_states()
-    val_loss.reset_states()
- 
-    print("\nStart of epoch %d" % (epoch + 1))
- 
-    start_time = time()
- 
-    # Iterate over the dataset batches
-    for step, (train_batchX, train_batchY) in enumerate(train_dataset):
- 
-        # Define the encoder and decoder inputs, and the decoder output
-        encoder_input = train_batchX[:, 1:]
-        decoder_input = train_batchY[:, :-1]
-        decoder_output = train_batchY[:, 1:]
- 
-        train_step(encoder_input, decoder_input, decoder_output)
- 
-        if step % 50 == 0:
-            print(f'Epoch {epoch + 1} Step {step} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
- 
-    # Run a validation step after every epoch of training
-    for val_batchX, val_batchY in val_dataset:
- 
-        # Define the encoder and decoder inputs, and the decoder output
-        encoder_input = val_batchX[:, 1:]
-        decoder_input = val_batchY[:, :-1]
-        decoder_output = val_batchY[:, 1:]
- 
-        # Generate a prediction
-        prediction = training_model(encoder_input, decoder_input, training=False)
- 
-        # Compute the validation loss
-        loss = loss_fcn(decoder_output, prediction)
-        val_loss(loss)
- 
-    # Print epoch number and accuracy and loss values at the end of every epoch
-    print("Epoch %d: Training Loss %.4f, Training Accuracy %.4f, Validation Loss %.4f" % (epoch + 1, train_loss.result(), train_accuracy.result(), val_loss.result()))
- 
-    # Save a checkpoint after every epoch
-    if (epoch + 1) % 1 == 0:
- 
-        save_path = ckpt_manager.save()
-        print("Saved checkpoint at epoch %d" % (epoch + 1))
- 
-        # Save the trained model weights
-        training_model.save_weights("weights/wghts" + str(epoch + 1) + ".ckpt")
- 
-        train_loss_dict[epoch] = train_loss.result()
-        val_loss_dict[epoch] = val_loss.result()
- 
-# Save the training loss values
-with open('./train_loss.pkl', 'wb') as file:
-    dump(train_loss_dict, file)
- 
-# Save the validation loss values
-with open('./val_loss.pkl', 'wb') as file:
-    dump(val_loss_dict, file)
- 
-print("Total time taken: %.2fs" % (time() - start_time))
+def run(epochs):
+    """
+
+    Parameters
+    ----------
+    epochs : int
+        Number of epochs to train the model.
+        
+
+    Returns
+    -------
+    None
+        The function updates the trainable variables of the model.
+
+    """
+    for epoch in range(epochs):
+    
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        val_loss.reset_states()
+    
+        print("\nStart of epoch %d" % (epoch + 1))
+    
+        start_time = time()
+    
+        # Iterate over the dataset batches
+        for step, (train_batchX, train_batchY) in enumerate(train_dataset):
+    
+            # Define the encoder and decoder inputs, and the decoder output
+            encoder_input = train_batchX[:, 1:]
+            decoder_input = train_batchY[:, :-1]
+            decoder_output = train_batchY[:, 1:]
+    
+            train_step(encoder_input, decoder_input, decoder_output)
+    
+            if step % 50 == 0:
+                print(f'Epoch {epoch + 1} Step {step} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+    
+        # Run a validation step after every epoch of training
+        for val_batchX, val_batchY in val_dataset:
+    
+            # Define the encoder and decoder inputs, and the decoder output
+            encoder_input = val_batchX[:, 1:]
+            decoder_input = val_batchY[:, :-1]
+            decoder_output = val_batchY[:, 1:]
+    
+            # Generate a prediction
+            prediction = training_model(encoder_input, decoder_input, training=False)
+    
+            # Compute the validation loss
+            loss = loss_fcn(decoder_output, prediction)
+            val_loss(loss)
+    
+        # Print epoch number and accuracy and loss values at the end of every epoch
+        print("Epoch %d: Training Loss %.4f, Training Accuracy %.4f, Validation Loss %.4f" % (epoch + 1, train_loss.result(), train_accuracy.result(), val_loss.result()))
+    
+        # Save a checkpoint after every epoch
+        if (epoch + 1) % 1 == 0:
+    
+            save_path = ckpt_manager.save()
+            print("Saved checkpoint at epoch %d" % (epoch + 1))
+    
+            # Save the trained model weights
+            training_model.save_weights("weights/wghts" + str(epoch + 1) + ".ckpt")
+    
+            train_loss_dict[epoch] = train_loss.result()
+            val_loss_dict[epoch] = val_loss.result()
+    
+    # Save the training loss values
+    with open('./train_loss.pkl', 'wb') as file:
+        dump(train_loss_dict, file)
+    
+    # Save the validation loss values
+    with open('./val_loss.pkl', 'wb') as file:
+        dump(val_loss_dict, file)
+    
+    print("Total time taken: %.2fs" % (time() - start_time))
+
+if __name__ == '__main__':
+    run(epochs=epochs)
