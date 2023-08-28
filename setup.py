4bd8c6db46f5d964f801189948d20935fa23711a
hastur66 <chamathkaubandara@gmail.com>
2023-08-29 00:15:59 +0530

    build: update package information

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open('README.md', 'r') as f:
+      long_description = f.read()
+
 setup(
       author='Chamath Attanyaka',
       name='micronus',
-      version='0.0.3',
+      version='0.0.4',
       description='A light weight transformer model',
       packages=find_packages(include=['micronus', 'micronus.*']),
       install_requires=[
@@ -14,4 +17,7 @@ setup(
             'matplotlib>=3.3.2',
       ],
       python_requires='>=3.8',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       )
